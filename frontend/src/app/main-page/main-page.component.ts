28183b1620ac35502e6cd0ac800461a1ab639cb8
Alexa-Iordache <alexa.iordache2013@gmail.com>
2024-04-08 22:19:18 +0300

    removed unused code

--- frontend/src/app/main-page/main-page.component.ts
+++ frontend/src/app/main-page/main-page.component.ts
@@ -1,6 +1,5 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { RpcService } from '../services/rpc.service';
 
 @Component({
   selector: 'app-main-page',
@@ -8,53 +7,19 @@ import { RpcService } from '../services/rpc.service';
   styleUrls: ['./main-page.component.scss'],
 })
 export class MainPageComponent {
-  @ViewChild('brightnessValue') brightnessValue: any;
-  @ViewChild('contrastValue') contrastValue: any;
 
-  // images' path
-  originalImagePath = '';
-  imageSegmentationPath = '';
-  imgBrightnessEnhPath = '';
-  imgSharpnessEnhPath = '';
-  imgFinalPath = '';
+  constructor(private router: Router) {}
 
-  hasTumor = false; // tumor detected ot not
-
-  resultAfterProcessing = 0; // contour procent resulted after image processing
-
-  resultButtonClicked = false;
-  finalImageButton = false;
-  brightness =  0;
-  contrast = 0;
-  isDisabled = true; // processing buttons are disabled until an image is uploaded
-  resultButtonsDisabled= true; // buttons for final result are disabled until sharpness button is clicked
-
-  constructor(private router: Router, private rpcService: RpcService) {}
-
-  // uploading an image
-  fileUpload(event: any) {
-    let variable = [];
-    variable = event.value.filename.split('\\');
-    this.originalImagePath = variable[2];
-    this.isDisabled = false; // the buttons are no longer disabled
+  trainModel(): void {
+    console.log('Train new model');
   }
 
-  // // reseting images -> in order to process another image
-  // resetButton() {
-  //   window.location.reload();
-  // }
-
-  antrenareModel(): void {
-    console.log('antrenare model');
-  }
-
-  utilizareModel(): void {
-    console.log('utilizare model');
+  useModel(): void {
+    console.log('Use existing model');
   }
 
   // exit function - returns to login page
   exitButton(): void {
-    this.resultButtonClicked = false;
     let copyInstance = this; // a copy of this class (atributes + methods)
     copyInstance.router.navigate(['/login']);
   }
